339edac614d497b743e10556dccdd817bb1d9452
Ahmed-Abdella <abdellaa921@gmail.com>
2023-02-04 20:39:59 +0200

    ifh

--- components/posts/post-item.tsx
+++ components/posts/post-item.tsx
@@ -1,55 +1,15 @@
 import Image from "next/image";
 import Link from "next/link";
-import PostType from "../../interfaces/post-type";
+
 import { PostDataType } from "../../interfaces/post-data";
 import Tags from "../tags";
-
-// import { Roboto_Mono } from "@next/font/google";
-
-// interface PostType {
-//   slug: string;
-//   id: number;
-//   title: string;
-//   description: string;
-//   imageURL: string;
-//   author: string;
-//   tags: string[];
-// }
-
-// const robotoMono = Roboto_Mono({
-//   weight: ["500"],
-//   subsets: ["latin"],
-// });
-
-// ${robotoMono.className} in the h3 tag
-
-// IMAGE PLACE HOLDER
-
-// const shimmer = (w: number, h: number) => `
-// <svg width="${w}" height="${h}" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-//   <defs>
-//     <linearGradient id="g">
-//       <stop stop-color="#333" offset="20%" />
-//       <stop stop-color="#222" offset="50%" />
-//       <stop stop-color="#333" offset="70%" />
-//     </linearGradient>
-//   </defs>
-//   <rect width="${w}" height="${h}" fill="#333" />
-//   <rect id="r" width="${w}" height="${h}" fill="url(#g)" />
-//   <animate xlink:href="#r" attributeName="x" from="-${w}" to="${w}" dur="1s" repeatCount="indefinite"  />
-// </svg>`;
-
-// const toBase64 = (str: string) =>
-//   typeof window === "undefined"
-//     ? Buffer.from(str).toString("base64")
-//     : window.btoa(str);
 
 export default function PostItem({ post }: { post: PostDataType }) {
   const myLoader = (): string => {
     return post.imageURL;
   };
   return (
-    <div className="h-full flex flex-col shadow-xl hover:scale-105  focus:scale-105 hover:md:transform-none transition duration-300 border  rounded-3xl">
+    <div className="h-full flex flex-col shadow-xl hover:-translate-y-4  focus:scale-105 hover:md:transform-none transition duration-300 border  rounded-3xl">
       <Link href={`/blog/${post.slug}`}>
         <div className="relative h-52  width-full ">
           <Image
